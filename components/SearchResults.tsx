a35fe1e0c4ced071a0cc6bdab7dae82684ad6fad
Frank Laércio <franklaercio@gmail.com>
2021-09-24 22:23:02 -0300

    Adding formatted data

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -2,7 +2,13 @@ import { useMemo } from "react";
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
-  results: Array<{ id: number; price: number; title: string }>;
+  totalPrice: number;
+  results: Array<{
+    id: number;
+    price: number;
+    title: string;
+    priceFormatted: string;
+  }>;
   onAddToWishlist: (id: number) => void;
 }
 
